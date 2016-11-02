e8c68bdee983dcf40f445d4330a343a87fbcfcd2
charlydes <c.desbaux@gmail.com>
2016-11-02 17:38:45 +0000

    no notes error message

--- kantanotes/notes/views.py
+++ kantanotes/notes/views.py
@@ -12,13 +12,14 @@ def index(request):
 def displayNotes(request, Categories_Id):
     categorieslist = Categories.objects.all()
     category = get_object_or_404(Categories, pk = Categories_Id)
+
     try:
         noteslist = get_list_or_404(Notes, categoryId = Categories_Id)
         context = {'categorieslist': categorieslist,'noteslist':noteslist}
         return render(request, 'notes/notes.html', context)
 
-    except Notes.DoesNotExist:
-        context = {'categorieslist': categorieslist,'errorMessage':"No notes in this category."}
+    except:
+        context = {'categorieslist': categorieslist, 'errorMessage':"No notes in this category."}
         return render(request, 'notes/notes.html', context)
 
 

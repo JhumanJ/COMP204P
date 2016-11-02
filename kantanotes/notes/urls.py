7f8b954ba6d17b8ea67837175ddb602400e80530
charlydes <c.desbaux@gmail.com>
2016-11-02 14:18:54 +0000

    Firsr push of charly branch

--- kantanotes/notes/urls.py
+++ kantanotes/notes/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
-from notes.models import Categories
+from notes.models import Categories, Notes
+from . import views
 
-urlpatterns = [url(r'^$', ListView.as_view(queryset=Categories.objects.all(),
-	template_name="notes/home.html"))]
+urlpatterns = [url(r'^$', views.index, name='index')]
+
+
+# urlpatterns = [url(r'^$', ListView.as_view(queryset=Notes.objects.all(),
+# 	template_name="notes/notes.html"))]
--- kantanotes/notes/views.py
+++ kantanotes/notes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Categories, Notes, Links 
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    categorieslist = Categories.objects.all()
+    noteslist = Notes.objects.all()
+    context = {'categorieslist': categorieslist,'noteslist':noteslist}
+    return render(request, 'notes/categories.html', context)

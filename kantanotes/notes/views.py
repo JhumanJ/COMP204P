c674556fff709a343f1ccbc87b3b6766036aa34e
charlydes <c.desbaux@gmail.com>
2016-11-02 17:06:21 +0000

    deleting cat

--- kantanotes/notes/views.py
+++ kantanotes/notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from .models import Categories, Notes, Links 
 from django.http import HttpResponse
 # Create your views here.
@@ -12,6 +12,18 @@ def index(request):
 def displayNotes(request, Categories_Id):
     categorieslist = Categories.objects.all()
     category = get_object_or_404(Categories, pk = Categories_Id)
-    noteslist = get_list_or_404(Notes, categoryId = Categories_Id)
-    context = {'categorieslist': categorieslist,'noteslist':noteslist}
-    return render(request, 'notes/notes.html', context)
+    try:
+        noteslist = get_list_or_404(Notes, categoryId = Categories_Id)
+        context = {'categorieslist': categorieslist,'noteslist':noteslist}
+        return render(request, 'notes/notes.html', context)
+
+    except Notes.DoesNotExist:
+        context = {'categorieslist': categorieslist,'errorMessage':"No notes in this category."}
+        return render(request, 'notes/notes.html', context)
+
+
+def deleteCategory(request, Categories_Id):
+    categorieslist = Categories.objects.all()
+    category = get_object_or_404(Categories, pk = Categories_Id)
+    category.delete()
+    return redirect('notes:index')
